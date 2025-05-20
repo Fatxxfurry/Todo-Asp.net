1121925a1799c230cf64b198f3d78713868c77b7
Hung <ares01202975777@gmail.com>
2025-05-20 23:38:35 +0700

    add unique to some column

--- MyApi/Data/AppDbContext.cs
+++ MyApi/Data/AppDbContext.cs
@@ -11,4 +11,12 @@ public class AppDbContext : DbContext
     public DbSet<Todo> Todos { get; set; }
     public DbSet<Tag> Tags { get; set; }
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().ToTable("users").HasIndex(u => u.email).IsUnique();
+        modelBuilder.Entity<Todo>().ToTable("todos");
+        modelBuilder.Entity<Tag>().ToTable("tags").HasIndex(t => t.name).IsUnique();
+        modelBuilder.Entity<Category>().ToTable("categories").HasIndex(c => c.name).IsUnique();
+    }
 }
